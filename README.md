b3d79a40376157ab2d9f70cb8e10246d86841e54
Maroš Varchola <marosvarchola@sunray.sk>
2022-08-19 14:31:32 +0200

    Initial_commit/SIW-299 Follow up for RELEASE notes prototype

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# maven-release-notes
-Release notes script for maven projects
+# Maven release notes script
+Release notes script for maven projects which is working based on pom.xml file in current folder.
+
+## How to run?
+* Run python3 release-notes.py
+* Run through based on instructions
+* On the end, the script will generate two files:
+** First, 'release_supp.data' for data of which release was already added to RELEASE.md
+** Second, 'RELEASE.md' for release notes itself
---
+++ release-notes.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+from xml.dom import minidom
+
+POMXML_PATH = "pom.xml"
+RELEASE_SUPPFILE_PATH = "release_supp.data"
+RELEASE_NOTES_PATH = "RELEASE.md"
+
+pomxml = minidom.parse(POMXML_PATH)
+
+xml_version = pomxml.getElementsByTagName('version')
+xml_name = pomxml.getElementsByTagName('name')
+
+app_ver = xml_version[0].firstChild.data
+app_name = xml_name[0].firstChild.data
+
+release_suppfile = open(RELEASE_SUPPFILE_PATH, "a+")
+release_suppfile.seek(0)
+
+already_done = release_suppfile.readlines()
+
+release_suppfile.close()
+
+print("RELEASE NOTE GENERATOR")
+print("Generating release notes for app '" +
+      app_name+"' with version '"+app_ver+"'")
+
+app_ver_full = app_ver+"\n"
+
+if app_ver_full in already_done:
+    print("Release note already added! Quitting")
+    quit()
+
+print("Please write release notes (End with Ctrl-D):")
+release_notes_raw = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    release_notes_raw.append(line)
+
+release_notes = ""
+
+for note in release_notes_raw:
+    release_notes += "* "+note+"\n"
+
+# Datetime
+now = datetime.now()
+dt_string = now.strftime("Datetime: %d-%m-%Y %H:%M:%S")
+
+append_text = "# Release version '"+app_ver+"'\n"+dt_string+"\n"+release_notes
+
+releasemd = open(RELEASE_NOTES_PATH, "a")
+
+try:
+    releasemd.write(append_text+"\n\n")
+except IOError:
+    print("Error writing to RELEASE.md!")
+else:
+    print("Writing to RELEASE.md OK!")
+    release_suppfile = open("release_supp.data", "a")
+    try:
+        release_suppfile.write(app_ver+"\n") == 0
+    except IOError:
+        print("Error writing to support file!")
+    else:
+        print("Writing to support file OK!")
+    release_suppfile.close()
+
+
+releasemd.close()

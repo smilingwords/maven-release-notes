4ca1d110badd86f9c3e2af5c18a3a9b770811c37
Robert Huppmann <robert.huppmann@smilingwords.eu>
2022-09-23 00:44:24 +0200

    Changed Python script release-notes.py and README.md file

--- README.md
+++ README.md
@@ -2,10 +2,28 @@
 Release notes script for maven projects which is working based on pom.xml file in current folder.
 Uses pom.xml file to generate structured release notes (RELEASE.md) file
 
+## Release notes workflow
+* Release notes are generated automatically by script
+* As result script generates two files: RELEASE.md and release_supp.data
+* RELEASE.md file and release_supp.data file are added to be commited automatically by script
+* RELEASE.md file and release_supp.data file are commited and pushed automatically by script
+
 ## How to run?
 * Copy release-notes.py file to the folder where pom.xml exists
+* Configure pom.xml file to commit and push generated files automatically by script:
+  * add and configure profile with id 'release' into profiles
+  * example of pom.xml configuration:
+
+      <profile>
+          <id>release</id>
+          <properties>
+              <message>Vcs: Release</message>
+              <use-ticket>true</use-ticket>
+          </properties>
+      </profile>
+
 * Run ```python3 release-notes.py```
 * Run through based on instructions
 * On the end, the script will generate two files:
-    * First, 'release_supp.data' for data of which release was already added to RELEASE.md
-    * Second, 'RELEASE.md' for release notes itself
+  * First, 'release_supp.data' for data of which release was already added to RELEASE.md
+  * Second, 'RELEASE.md' for release notes itself
--- release-notes.py
+++ release-notes.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from xml.dom import minidom
+import subprocess
 
 POMXML_PATH = "pom.xml"
 RELEASE_SUPPFILE_PATH = "release_supp.data"
@@ -9,11 +10,20 @@ pomxml = minidom.parse(POMXML_PATH)
 
 xml_version = pomxml.getElementsByTagName('version')
 xml_name = pomxml.getElementsByTagName('name')
+xml_message = pomxml.getElementsByTagName('message')
+xml_use_ticket = pomxml.getElementsByTagName('use-ticket')
 
 app_ver = xml_version[0].firstChild.data
 app_name = xml_name[0].firstChild.data
+app_msg = ""
+app_use_tckt = ""
 
-app_ver_num, app_ver_suffix = app_ver.split('-')
+if xml_message:
+    app_msg = xml_message[0].firstChild.data
+if xml_use_ticket:
+    app_use_tckt = xml_use_ticket[0].firstChild.data
+
+app_ver_numbr, app_ver_suffix = app_ver.split('-')
 
 release_suppfile = open(RELEASE_SUPPFILE_PATH, "a+")
 release_suppfile.seek(0)
@@ -24,9 +34,9 @@ release_suppfile.close()
 
 print("RELEASE NOTE GENERATOR")
 print("Generating release notes for app '" +
-      app_name+"' with version '"+app_ver_num+"'")
+      app_name+"' with version '"+app_ver_numbr+"'")
 
-app_ver_full = app_ver_num+"\n"
+app_ver_full = app_ver_numbr+"\n"
 
 if app_ver_full in already_done:
     print("Release note already added! Quitting")
@@ -50,7 +60,7 @@ for note in release_notes_raw:
 now = datetime.now()
 dt_string = now.strftime("Datetime: %d-%m-%Y %H:%M:%S")
 
-append_text = "# Release version '"+app_ver_num+"'\n"+dt_string+"\n"+release_notes
+append_text = "# Release version '"+app_ver_numbr+"'\n"+dt_string+"\n"+release_notes
 
 releasemd = open(RELEASE_NOTES_PATH, "a")
 
@@ -62,7 +72,7 @@ else:
     print("Writing to RELEASE.md OK!")
     release_suppfile = open("release_supp.data", "a")
     try:
-        release_suppfile.write(app_ver_num+"\n") == 0
+        release_suppfile.write(app_ver_numbr+"\n") == 0
     except IOError:
         print("Error writing to support file!")
     else:
@@ -70,3 +80,22 @@ else:
     release_suppfile.close()
 
 releasemd.close()
+
+message_text = ""
+if xml_message:
+    if app_use_tckt == "true":
+        print("Please write ticket and press Enter")
+        ticket_txt = input()
+        message_text = app_msg+app_ver_numbr+", "+ticket_txt
+    elif app_use_tckt == "false":
+        message_text = app_msg+app_ver_numbr
+
+    subprocess.run(["git", "add", "RELEASE.md"])
+    subprocess.run(["git", "add", "release_supp.data"])
+    subprocess.run(["git", "commit", "-m", message_text])
+    subprocess.run(["git", "push"])
+
+else:
+    print("No message template defined in pom.xml file!")
+    print("Perform commit of release manually!")
+    quit()

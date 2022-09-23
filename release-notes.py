3962523f32e87b53d303c3701cd297556341ee0b
Robert Huppmann <robert.huppmann@smilingwords.eu>
2022-09-23 12:46:08 +0200

    Changed Python script release-notes.py

--- release-notes.py
+++ release-notes.py
@@ -13,17 +13,17 @@ xml_name = pomxml.getElementsByTagName('name')
 xml_message = pomxml.getElementsByTagName('message')
 xml_use_ticket = pomxml.getElementsByTagName('use-ticket')
 
-app_ver = xml_version[0].firstChild.data
+app_version = xml_version[0].firstChild.data
 app_name = xml_name[0].firstChild.data
-app_msg = ""
-app_use_tckt = ""
+app_message = ""
+app_use_ticket = ""
 
 if xml_message:
-    app_msg = xml_message[0].firstChild.data
+    app_message = xml_message[0].firstChild.data
 if xml_use_ticket:
-    app_use_tckt = xml_use_ticket[0].firstChild.data
+    app_use_ticket = xml_use_ticket[0].firstChild.data
 
-app_ver_numbr, app_ver_suffix = app_ver.split('-')
+app_version_number, app_version_suffix = app_version.split('-')
 
 release_suppfile = open(RELEASE_SUPPFILE_PATH, "a+")
 release_suppfile.seek(0)
@@ -34,11 +34,11 @@ release_suppfile.close()
 
 print("RELEASE NOTE GENERATOR")
 print("Generating release notes for app '" +
-      app_name+"' with version '"+app_ver_numbr+"'")
+      app_name+"' with version '"+app_version_number+"'")
 
-app_ver_full = app_ver_numbr+"\n"
+app_version_full = app_version_number+"\n"
 
-if app_ver_full in already_done:
+if app_version_full in already_done:
     print("Release note already added! Quitting")
     quit()
 
@@ -60,7 +60,7 @@ for note in release_notes_raw:
 now = datetime.now()
 dt_string = now.strftime("Datetime: %d-%m-%Y %H:%M:%S")
 
-append_text = "# Release version '"+app_ver_numbr+"'\n"+dt_string+"\n"+release_notes
+append_text = "# Release version '"+app_version_number+"'\n"+dt_string+"\n"+release_notes
 
 releasemd = open(RELEASE_NOTES_PATH, "a")
 
@@ -72,7 +72,7 @@ else:
     print("Writing to RELEASE.md OK!")
     release_suppfile = open("release_supp.data", "a")
     try:
-        release_suppfile.write(app_ver_numbr+"\n") == 0
+        release_suppfile.write(app_version_numbr+"\n") == 0
     except IOError:
         print("Error writing to support file!")
     else:
@@ -81,21 +81,21 @@ else:
 
 releasemd.close()
 
-message_text = ""
+# default value
+message_text = app_message+app_version_number
+
 if xml_message:
     if app_use_tckt == "true":
         print("Please write ticket and press Enter")
         ticket_txt = input()
-        message_text = app_msg+app_ver_numbr+", "+ticket_txt
-    elif app_use_tckt == "false":
-        message_text = app_msg+app_ver_numbr
+        message_text = app_message+app_version_number+", "+ticket_txt
 
-    subprocess.run(["git", "add", "RELEASE.md"])
-    subprocess.run(["git", "add", "release_supp.data"])
+    subprocess.run(["git", "add", "RELEASE.md", "release_supp.data"])
     subprocess.run(["git", "commit", "-m", message_text])
     subprocess.run(["git", "push"])
 
 else:
     print("No message template defined in pom.xml file!")
-    print("Perform commit of release manually!")
+    print("To configure pom.xml follow steps in part How to run? in README file)")
+    print("Now perform commit of release notes manually!")
     quit()
